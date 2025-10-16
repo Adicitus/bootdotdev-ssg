9ce9f9d4a94675e9afcf3d2c28d2c9187fb63742
Joakim Olsson <joakim.olsson@adicitus.cloud>
2025-10-16 15:29:42 +0000

    reorganized and added a general function to parse markdown

--- src/parse_markdown.py
+++ src/parse_markdown.py
@@ -44,4 +44,82 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type:TextType):
     
     return new_nodes
 
-        
+def split_nodes_image(old_nodes:list) -> list:
+    new_nodes = []
+
+    for node in old_nodes:
+        matches = re.finditer(r"!\[(?P<alt>[^\]]*)\]\((?P<url>[^\)]*)\)", node.text)
+        last_index = 0
+        for match in matches:
+            start_index = match.start()
+            end_index = match.end()
+
+            if last_index < start_index:
+                new_nodes.append(TextNode(node.text[last_index:start_index], node.text_type, node.url))
+            
+            new_nodes.append(TextNode(match.group('alt'), TextType.IMAGE, match.group('url')))
+            last_index = end_index
+
+    return new_nodes
+
+def split_nodes_link(old_nodes:list) -> list:
+    new_nodes = []
+
+    for node in old_nodes:
+        matches = re.finditer(r"\[(?P<anchor>[^\]]*)\]\((?P<url>[^\)]*)\)", node.text)
+        last_index = 0
+        for match in matches:
+            start_index = match.start()
+            end_index = match.end()
+
+            if last_index < start_index:
+                new_nodes.append(TextNode(node.text[last_index:start_index], node.text_type, node.url))
+            
+            new_nodes.append(TextNode(match.group('anchor'), TextType.LINK, match.group('url')))
+            last_index = end_index
+
+    return new_nodes
+
+patterns = {
+    TextType.BOLD:      r"(?P<el_bold>\*\*(?P<bold_text>[^*]+)\*\*)",
+    TextType.ITALIC:    r"(?P<el_italic>_(?P<italic_text>[^_]+)_)",
+    TextType.CODE:      r"(?P<el_code>`(?P<code_text>[^`]+)`)",
+    TextType.LINK:      r"(?P<el_link>\[(?P<anchor>[^\]]*)\]\((?P<link_url>[^\)]*)\))",
+    TextType.IMAGE:     r"(?P<el_image>!\[(?P<alt>[^\]]*)\]\((?P<image_url>[^\)]*)\))"
+}
+
+patterns['all'] = f"({"|".join(patterns.values())})"
+
+def split_nodes(old_nodes:list) -> list:
+    new_nodes = []
+    for node in old_nodes:
+        matches = re.finditer(patterns['all'], node.text)
+        last_index = 0
+        for match in matches:
+            start_index = match.start()
+            end_index = match.end()
+            if last_index < start_index:
+                new_nodes.append(TextNode(node.text[last_index:start_index], node.text_type, node.url))
+            
+            values = match.groupdict()
+            node_type = list(filter(lambda k: k.startswith("el_") and values[k] != None, values.keys()))[0].split('_')[1]
+
+            match node_type:
+                case "bold":
+                    new_nodes.append(TextNode(values['bold_text'], TextType.BOLD))
+                case "italic":
+                    new_nodes.append(TextNode(values['italic_text'], TextType.ITALIC))
+                case "code":
+                    new_nodes.append(TextNode(values['code_text'], TextType.CODE))
+                case "link":
+                    new_nodes.append(TextNode(values['anchor'], TextType.LINK, values['link_url']))
+                case "image":
+                    new_nodes.append(TextNode(values['alt'], TextType.IMAGE, values['image_url']))
+            
+            last_index = end_index
+
+    return new_nodes
+
+def text_to_textnodes(text:str) -> list:
+    node = TextNode(text, TextType.PLAIN)
+    return split_nodes([node])
