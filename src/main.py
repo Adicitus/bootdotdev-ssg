145c300c1b6f23b40beea7cf105a3808cd906a06
Joakim Olsson <joakim.olsson@adicitus.cloud>
2025-10-18 18:39:37 +0000

    Added regeneration of the public directory.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,57 @@
+import sys
+import os
+import shutil
+
 from textnode import TextNode, TextType
 
-def main():
+def main(force_regen=False):
     node = TextNode(
         "This is some anchor text",
         TextType.LINK,
         "http://0.0.0.0:8888"
     )
-    print(node)
+
+    src_folder = os.path.dirname(__file__)
+    project_folder  = os.path.abspath(os.path.join(src_folder, ".."))
+    public_folder   = os.path.join(project_folder, "public")
+    static_folder   = os.path.join(project_folder, "static")
+
+    print(f"{project_folder}: {os.path.exists(project_folder)}")
+    print(f"{public_folder}: {os.path.exists(public_folder)}")
+    print(f"{static_folder}: {os.path.exists(static_folder)}")
+
+    generate = not os.path.exists(public_folder) or (force_regen and os.path.isdir(public_folder))
+
+    if generate and os.path.exists(public_folder):
+        print(f"Removing existing public folder: {projec_folder}")
+        shutil.rmtree(public_folder)
+
+    if generate:
+        # Copy static assets
+        def copy_files(src_root, dst_root):
+            for item in os.scandir(src_root):
+                if item.is_dir():
+                    new_src_path = os.path.join(src_root, item.name)
+                    new_dst_path = os.path.join(dst_root, item.name)
+                    print(f"Creating new directory: {new_dst_path}")
+                    os.mkdir(new_dst_path)
+                    copy_files(new_src_path, new_dst_path)
+                else:
+                    src_path = os.path.join(src_root, item.name)
+                    dst_path = os.path.join(dst_root, item.name)
+                    print(f"Copying: {src_path} -> {dst_path}")
+                    shutil.copy(src_path, dst_path)
+        print(f"Creating public folder ({public_folder})")
+        os.mkdir(public_folder)
+        print("Copying static files...")
+        copy_files(static_folder, public_folder)
+        # Generate HTML
+        pass
+
 
 if __name__ == "__main__":
-    main()
+    print(sys.argv)
+    regenerate = "-f" in sys.argv or "--force" in sys.argv
+    print(regenerate)
+    main(force_regen=regenerate)
+
